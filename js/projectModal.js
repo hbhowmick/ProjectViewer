315f0b2ab82292ca4635433964b22c16aa363443
hbhowmick <44176318+hbhowmick@users.noreply.github.com>
2019-12-13 09:48:14 -0500

    Initial Commit - Project Viewer

---
+++ js/projectModal.js
@@ -0,0 +1,14 @@
+var projectModal = document.getElementById("projectModal");
+
+var projectBtn = document.getElementById("projectBtn");
+
+var closeProject = document.getElementsByClassName("closeProject")[0];
+
+// To re-open modal, click the About button in the top right
+projectBtn.onclick = function() {
+  projectModal.style.display = "block";
+}
+
+closeProject.onclick = function() {
+  projectModal.style.display = "none";
+}
